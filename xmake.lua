fba12b02e385ebf9a61da9b41c2a28e163795317
Liu <lc-soft@live.cn>
2019-12-04 20:36:31 +0800

    build(xmake): add coverage flags

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 add_rules("mode.debug", "mode.release", "mode.coverage")
 add_linkdirs("/usr/local/lib")
+set_warnings("all", "error")
 
 lcpkg_dir = "./lcpkg/installed/$(arch)-$(os)"
 lcpkg_incdir = lcpkg_dir.."/include"
@@ -8,7 +9,6 @@ if is_mode("debug") then
     lcpkg_pkgdir = lcpkg_dir.."/debug"
 end
 lcpkg_libdir = lcpkg_pkgdir.."/lib"
-
 add_includedirs("include", lcpkg_incdir)
 add_linkdirs(lcpkg_libdir, "/usr/local/lib", "/usr/lib")
 
@@ -17,6 +17,15 @@ target("LCUIRouter")
     add_files("src/*.c")
 
 target("test")
+    if is_plat("linux") and is_mode("coverage") then
+        add_cflags("-ftest-coverage", "-fprofile-arcs", {force = true})
+        add_links("gcov")
+        after_run(function (target)
+            os.run("lcov --test-name test --output-file test/test.info --directory build --capture")
+            os.run("genhtml -o test/test.report test/test.info")
+            os.run("xdg-open test/test.report/index.html")
+        end)
+    end
     set_kind("binary")
     add_files("test/test.c")
     add_links("LCUI")
